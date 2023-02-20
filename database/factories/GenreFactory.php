d6cf7c15cbd047e72bfb9944b10b54963cabae8f
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-20 15:42:17 +0100

    Create faker on genre and movie factory

--- database/factories/GenreFactory.php
+++ database/factories/GenreFactory.php
@@ -17,7 +17,8 @@ class GenreFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => fake()->words(rand(1, 3), true),
+            'description' => fake()->boolean() ? fake()->text() : null,
         ];
     }
 }
--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -12,12 +12,14 @@ class MovieFactory extends Factory
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array<string => fake() -> , mixed>
      */
     public function definition()
     {
         return [
-            //
+            'name' => fake()->words(rand(1, 3), true),
+            'year' => fake()->year('now'),
+            'cashOut' => fake()->numberBetween(100000000, 1000000000000),
         ];
     }
 }
