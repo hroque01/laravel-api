415b13732042d0f7a6e3775c5b041197f15e6e3d
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-20 16:01:42 +0100

    Create MovieSeeder

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Genre;
+use App\Models\Movie;
+use App\Models\Tag;
+
 class MovieSeeder extends Seeder
 {
     /**
@@ -14,6 +18,14 @@ class MovieSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Movie::factory()->count(100)->make()->each(function ($m) {
+
+            $genre = Genre::inRandomOrder()->first();
+            $m->genre()->associate($genre);
+            $m->save();
+
+            $tags = Tag::inRandomOrder()->limit(rand(1, 5))->get();
+            $m->tags()->sync($tags);
+        });
     }
 }
