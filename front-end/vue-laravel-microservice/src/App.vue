137fdee40f7b1aa146ea42ba96b4a20869383bff
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-23 22:28:18 +0100

    Print movies with vue

--- front-end/vue-laravel-microservice/src/App.vue
+++ front-end/vue-laravel-microservice/src/App.vue
@@ -2,49 +2,37 @@
 import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      movies: [],
+      genre: [],
+      tags: [],
+    }
+  },
   mounted() {
     axios.get('http://localhost:8000/api/v1/test')
       .then(res => {
         const data = res.data;
         const success = res.success;
-        const movies = data.response;
+        const response = data.response;
+        const genres = response.genre;
 
-        console.log(movies)
+        this.movies = response.movies;
+        this.genres = genres;
+
+
+
+        console.log(this.movies)
       })
       .catch(err => console.error(err));
-  }
+  },
 }
 </script>
 
 <template>
-  <h1> Hello World DA VUEE DAJE</h1>
+  <div v-for="movie in movies" :key="movie.id">
+    {{ movie.name }}
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
